4485c4ce8140f8126b86091a0e0ed09b130f4e76
Andrew Downie <andrew_dev@outlook.com>
2017-08-31 17:49:30 -0400

    Improvements to timer input cleaning.

--- js/index.js
+++ js/index.js
@@ -24,4 +24,55 @@ $(document).ready(function(){
     $(".project-choice").click(function(){
         $("#selected-project").val($(this).text());
     });
+
+
+    ///
+    /// Time Input Changed
+    ///
+    $("#hours, #minutes, #seconds").on('keydown mousedown keyup mouseup', function(){
+        UpdateTimerInputs();
+    });
+
+    $("#hours, #minutes, #seconds").focusout(function(){
+        if($("#hours").val().length == 0){
+            $("#hours").val("0");
+        }
+        if($("#minutes").val().length == 0){
+            $("#minutes").val("0");
+        }
+        if($("#seconds").val().length == 0){
+            $("#seconds").val("0");
+        }
+    });
+
 });
+
+function UpdateTimerInputs(){
+    var hours = $("#hours").val();
+    var minutes = $("#minutes").val();
+    var seconds = $("#seconds").val();
+
+    if(hours > 12){
+        $("#hours").val("12");
+        hours = 12;
+    }
+
+    if(minutes > 59){
+        $("#minutes").val("59");
+        minutes = 59;
+    }
+
+    if(seconds > 59){
+        $("#seconds").val("59");
+        seconds = 59;
+    }
+
+
+
+    hours = hours.toString().padStart(1, "0");
+    minutes = minutes.toString().padStart(2, "0");
+    seconds = seconds.toString().padStart(2, "0");
+
+
+    $("#time-remaining").text(hours + ":" + minutes + ":" + seconds);
+}
